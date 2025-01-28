6019ca9b78d93bdac4d4f46a426b0c3dabbeb54b
Kasra Kamal <kasrakamal@microsoft.com>
2025-01-28 10:24:09 -0800

    Update set_args to support netlog

--- utils/set_args.ps1
+++ utils/set_args.ps1
@@ -2,32 +2,47 @@
 # Licensed under the BSD-3-Clause license.
 
 if ($ARGS[0] -eq $null) {exit 1}
-if ($ARGS[1] -eq $null) {exit 1}
-if ($ARGS[2] -eq $null) {exit 1}
 
-$browser = "Microsoft Edge"
+$browser = "edge"
 
-if ($ARGS.Length -gt 3) {
-    switch ($ARGS[3]) {
-        "edgedev" {
-            $browser = "Microsoft Edge Dev"
-        }
-        "edgebeta" {
-            $browser = "Microsoft Edge Beta"
-        }
-        "edgecanary" {
-            $browser = "Microsoft Edge Canary"
-        }
-        "chrome" {
-            $browser = "Google Chrome"
-        }
+if ($ARGS[0] -eq "web_replay") {
+    if ($ARGS[1] -eq $null) {exit 1}
+    if ($ARGS[2] -eq $null) {exit 1}
+    if ($ARGS[3] -eq $null) {exit 1}
+    if ($ARGS[4] -ne $null) {$browser = $ARGS[4]}
+
+    $HostHTTP = "$($ARGS[1]):$($ARGS[2])"
+    $HostHTTPS = "$($ARGS[1]):$($ARGS[3])"
+
+    $Arguments = "--host-resolver-rules=`"MAP *:80 $HostHTTP,MAP *:443 $HostHTTPS`""
+} elseif ($ARGS[0] -eq "live") {
+    if ($ARGS[1] -eq $null) {exit 1}
+    if ($ARGS[2] -ne $null) {$browser = $ARGS[2]}
+
+    $Arguments = "--log-net-log=$($ARGS[1]) --disable-quic"
+} else {
+    exit 1
+}
+
+switch ($browser) {
+    "edge" {
+        $browser = "Microsoft Edge"
+    }
+    "edgedev" {
+        $browser = "Microsoft Edge Dev"
+    }
+    "edgebeta" {
+        $browser = "Microsoft Edge Beta"
+    }
+    "edgecanary" {
+        $browser = "Microsoft Edge Canary"
+    }
+    "chrome" {
+        $browser = "Google Chrome"
     }
 }
 
 $WshShell = New-Object -ComObject ("WScript.Shell")
-
-$HostHTTP = "$($ARGS[0]):$($ARGS[1])"
-$HostHTTPS = "$($ARGS[0]):$($ARGS[2])"
 
 $ShortcutPath = "$($env:APPDATA)\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\$browser.lnk"
 
@@ -36,7 +51,7 @@ if (-not (Test-Path $ShortcutPath)) {
 }
 
 $Shortcut = $WshShell.CreateShortcut($ShortcutPath)
-$Shortcut.Arguments = "--host-resolver-rules=`"MAP *:80 $HostHTTP,MAP *:443 $HostHTTPS`""
+$Shortcut.Arguments = $Arguments
 $Shortcut.Save()
 
 exit
