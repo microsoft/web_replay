79424d47506e0885aafd791cb984f926baf668cc
Kasra Kamal <kasrakamal@microsoft.com>
2025-02-20 10:49:17 -0800

    Update google deterministic file

--- deterministic/google_deterministic.js
+++ deterministic/google_deterministic.js
@@ -4,24 +4,16 @@
 'use strict';
 
 (function () {
-  function sfc32(a, b, c, d) {
-    return function() {
-      a |= 0; b |= 0; c |= 0; d |= 0;
-      var t = (a + b | 0) + d | 0;
-      d = d + 1 | 0;
-      a = b ^ b >>> 9;
-      b = c + (c << 3) | 0;
-      c = (c << 21 | c >>> 11);
-      c = c + t | 0;
-      return (t >>> 0) / 4294967296;
-    }
-  };
-  var seed = 1337 ^ 0xDEADBEEF;
-  var rand = sfc32(0x9E3779B9, 0x243F6A88, 0xB7E15162, seed);
-  // Mix initial states
-  for (var i = 0; i < 15; i++) rand();
+  var random_count = 0;
+  var random_count_threshold = 25;
+  var random_seed = 0.462;
   Math.random = function() {
-    return rand();
+    random_count++;
+    if (random_count > random_count_threshold){
+     random_seed += 0.1;
+     random_count = 1;
+    }
+    return (random_seed % 1);
   };
   if (typeof(crypto) == 'object' &&
       typeof(crypto.getRandomValues) == 'function') {
@@ -36,16 +28,16 @@
 })();
 (function () {
   var date_count = 0;
-  var date_count_threshold = 1;
+  var date_count_threshold = 25;
   var orig_date = Date;
   // Time since epoch in milliseconds. This is replaced by script injector with
   // the date when the recording is done.
-  var time_seed = {{WPR_TIME_SEED_TIMESTAMP}};
+  var time_seed = {{WPR_TIME_SEED_TIMESTAMP}} + 5000000;
   Date = function() {
     if (this instanceof Date) {
       date_count++;
       if (date_count > date_count_threshold){
-        time_seed += 1000000;
+        time_seed += 50;
         date_count = 1;
       }
       switch (arguments.length) {
@@ -66,15 +58,15 @@
   Date.prototype.constructor = Date;
   var inc = 0;
   orig_date.now = function() {
-    inc += 1;
+    inc += 0.001;
     return new Date().getTime() + inc;
   };
   orig_date.prototype.now = function() {
-    inc += 1;
+    inc += 0.001;
     return new Date().getTime() + inc;
   };
   Date.now = function() {
-    inc += 1;
+    inc += 0.001;
     return new Date().getTime() + inc;
   };
   orig_date.prototype.getTimezoneOffset = function() {
